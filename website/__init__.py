26685ddbaae94bca67145fa73714a61fcfb78f03
jurandarjanovic <darjan.juranovic@gmail.com>
2022-08-24 21:03:45 +0200

    ...

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) #inicijalizacija flaska
     app.config['SECRET KEY'] = 'lozinkajelozinka'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #ovdje se nalazi baza
+    db.init_app(app)
+    
     from .pogledi import pogledi #importam blueprint
     from .prijava import prijava
 
