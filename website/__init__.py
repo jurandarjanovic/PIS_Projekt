fa6c3beaed79f6c33ec98fefdbed8f24da65cff1
jurandarjanovic <darjan.juranovic@gmail.com>
2022-08-24 22:30:01 +0200

    ...

--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,40 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
-    app = Flask(__name__) #inicijalizacija flaska
-    app.config['SECRET KEY'] = 'lozinkajelozinka'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #ovdje se nalazi baza
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
-    
-    from .pogledi import pogledi #importam blueprint
-    from .prijava import prijava
 
-    app.register_blueprint(pogledi, url_prefix='/') #kako pristupam svakom url-u koji je u blueprintu
-    app.register_blueprint(prijava, url_prefix='/')
+    from .pogledi import views
+    from .autorizacija import auth
 
-    return app #aplikacija kreirana
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .modeli import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Baza kreirana!')
